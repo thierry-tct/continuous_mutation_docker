e18de4666426110e0673da433cf8c48c9a8eaee5
Thierry Titcheu Chekam <thierry_tct@yahoo.com>
2021-11-27 12:44:44 +0000

    update shadow - fixing missing

--- shadow/kleeDeploy/stp/src/main/versionString.cpp
+++
@@ -1,2 +0,0 @@
-#include <string>
-namespace BEEV{extern const std::string version = " 32:1668M ";}
--- shadow/kleeDeploy/stp/src/sat/utils/depend.mk
+++
@@ -1,8 +0,0 @@
-/home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/Options.o /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/Options.or /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/Options.od /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/Options.op: /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/Options.cc \
- ../mtl/Sort.h ../mtl/../mtl/Vec.h ../mtl/../mtl/../mtl/IntTypesMtl.h \
- ../mtl/../mtl/../mtl/XAlloc.h ../utils/Options.h \
- ../utils/../mtl/IntTypesMtl.h ../utils/../mtl/Vec.h \
- ../utils/../utils/ParseUtils.h ../utils/ParseUtils.h
-/home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/System.o /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/System.or /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/System.od /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/System.op: /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/System.cc \
- /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/../utils/System.h \
- /home/shadowvm/shadow/kleeDeploy/stp/src/sat/utils/../utils/../mtl/IntTypesMtl.h
